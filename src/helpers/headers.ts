bc90378fa987ac1df5737407bd520d68954df4af
Lin47 <1941585039@qq.com>
2019-07-05 00:45:10 +0800

    编写部分测试用例

--- src/helpers/headers.ts
+++ src/helpers/headers.ts
@@ -32,14 +32,17 @@ export function parseHeaders(headers: string): any {
   }
 
   headers.split('\r\n').forEach(line => {
-    let [key, val] = line.split(':')
+    // bug 修复
+    // let [key, val] = line.split(':')
+    let [key, ...vals] = line.split(':')
     key = key.trim().toLowerCase()
     if (!key) {
       return
     }
-    if (val) {
-      val = val.trim()
-    }
+    const val = vals.join(':').trim()
+    // if (val) {
+    //   val = val.trim()
+    // }
     parsed[key] = val
   })
   return parsed
